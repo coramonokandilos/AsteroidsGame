6f580c88b79ac1ec9b9618aa9c92d7aea247201e
coramonokandilos <cora.monokandilos@gmail.com>
2014-11-12 10:20:53 -0800

    added stars and fixed the ship

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip shaceship;
 Starz [] stars;
 
@@ -27,11 +43,11 @@ public void keyPressed()
 {
   if (keyCode == UP) 
   {
-    shaceship.accelerate(0.3);
+    shaceship.accelerate(0.3f);
 }
   else if (keyCode == DOWN) 
   {
-    shaceship.accelerate(-0.3);
+    shaceship.accelerate(-0.3f);
 
   }
   else if (keyCode == RIGHT)
@@ -49,8 +65,8 @@ class Starz
   private int myColor, myCenterX, myCenterY,  color2;
   Starz()
   {
-    color2 = #000000;
-    myColor = #FFFFFF;
+    color2 = 0xff000000;
+    myColor = 0xffFFFFFF;
     myCenterX = (int)(Math.random() *400);
     myCenterY = (int)(Math.random() *400);
   }
@@ -58,8 +74,8 @@ class Starz
   {
     double blink = Math.random();
     stroke(255);
-    strokeWeight(0.5);
-    if (blink < 0.5)
+    strokeWeight(0.5f);
+    if (blink < 0.5f)
     {
       fill(color2);
     }
@@ -94,7 +110,7 @@ class SpaceShip extends Floater
   yCorners[6] = 8;
   xCorners[7] = 6;
   yCorners[7] = 4;
-  myColor = #FF007F;
+  myColor = 0xffFF007F;
   myCenterX = 200;
   myCenterY = 200;
   myDirectionX = 0;
@@ -211,3 +227,12 @@ abstract class Floater
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
